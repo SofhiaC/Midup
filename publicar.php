c89e9a792351f28b2cfb74cc5826752591044cba
Fernanda Giacobbo <fernanda.giacobbo@pucpr.edu.br>
2024-11-16 15:48:56 -0300

    php publicar receita v2

--- publicar.php
+++ publicar.php
@@ -3,25 +3,26 @@
 $hostname = "localhost"; 
 $bancodedados = "revirado";
 $usuario = "root";
-$senha = "";
+$senha = "fereluna15";
 
-$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
 if($mysqli -> connect_errno){
     echo "Falha ao conectar: (". $mysqli->connect_errno . ")" .mysqli->connect_error;
 } else {
     echo "Conectado"
-}
+};
 
-
-$titulo = $_POST['nome'];
-$imagem_carregada = $_POST['imagem_carregada'];
+$titulo = $_POST['titulo'];
+$imagem = $_POST['imagem_carregada'];
 $introducao = $_POST['introducao'];
 $ingredientes = $_POST['ingredientes'];
 $modo_prep = $_POST['modo_prep'];
 $porcoes = $_POST['porcoes'];
 $dificuldade = $_POST['dificuldade'];
-$categorias = $_POST['cat'];
+$categoria = $_POST['cat'];
 $tempo_preparo = $_POST['tempo'];
+
+$result = mysqli_query($conexao, "INSERT INTO tb_usuario(tempo_preparo, dificuldade, tipo_receita, quant_porcoes, modo_de_preparo, nome_receita, imagem_receita) VALUES ('$tempo_preparo', '$dificuldade', '$categoria', '$porcoes', '$modo_prep', '$titulo', '$imagem')");
 
 ?>
